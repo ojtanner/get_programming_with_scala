c478caf5c760b9e8ac59a5e2bfff90a496990086
tanneoli <oliver-tanner@outlook.com>
2021-10-03 16:40:57 +0200

    Add First Half Of Classes

---
+++ src/main/scala/1_absolute_basics/8_Classes.scala
@@ -0,0 +1,57 @@
+package `1_absolute_basics`
+
+/**
+  * Classes should be familiar to you if you come from a Java
+  * background.
+  * Scala classes are the same concept as in Java, but they
+  * have slightly different syntax.
+  * 
+  */
+
+object Classes {
+  /**
+    * First of all: Classes do not need bodies in Scala.
+    */
+  class Robot(name: String)
+
+  val iRobot = new Robot("Will Smith")
+  // this will not work: iRobot.name
+
+  /**
+    * If you want to access the name of the Robot, you
+    * have to mark the parameter as value or variable.
+    * Why? Because it is a shorthand to declare them
+    * as public variables.
+    * See:
+    * link: https://docs.scala-lang.org/tour/classes.html
+    * chapter: Private Members and Getter/Setter Syntax
+    */
+  class AnotherRobot(val name: String)
+  val anotherRobot = new AnotherRobot("Henry")
+  print(anotherRobot.name)
+
+  /**
+    * As with functions, you can provide default args
+    * for constructor parameters.
+    */
+  class DefaultRobot(val name: String = "Defaulto Roboto")
+
+  /**
+    * You can declare functions inside classes. These functions
+    * are then called methods. But probably already knew that.
+    */
+  class GreeterRobot() {
+    def greet(name: String = "World"): Unit = {
+      println("Hello, " + name)
+    }
+  }
+
+  /**
+    * As with functions, you can provide constructor args as named
+    * args. This is a good idea if you provide multiple args
+    * that share the same type.
+    */
+  class TreasureChest(longitude: Int, latitude: Int)
+
+  val xMarksTheSPot = new TreasureChest(longitude = 42, latitude = 69)
+}
