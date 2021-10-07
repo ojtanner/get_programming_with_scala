6e7a105dc9c47990da613506fed21b78131d3bdb
tanneoli <oliver-tanner@outlook.com>
2021-10-07 22:19:27 +0200

    Stop At Page 97, Apply Not Yet Added

---
+++ src/main/scala/1_absolute_basics/11_SingletonObjects.scala
@@ -0,0 +1,23 @@
+package `1_absolute_basics`
+
+/**
+  * Singletons
+  * ---
+  * 
+  * In Scala, if you need a singleton, you can simply create
+  * it by creating an object. Just like the object down below.
+  * 
+  */
+
+object SingletonObjects {
+  
+}
+
+/**
+  * Companion Objects
+  * ---
+  * 
+  * A popular pattern of having both a class and an object
+  * with the same name. This means you can move all
+  * static methods into the object.
+  */
